a754e35d81255f5a8bff6d52d05c5c185ed5cffc
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-09-28 17:50:07 -0500

    ok

--- server/dist/index.js
+++ server/dist/index.js
@@ -16,7 +16,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, helmet_1.default)());
-app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
+//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
