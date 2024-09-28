37443db2bfe4bc6bd643fbd4f006d070a8a79c11
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-09-28 15:26:08 -0500

    ok

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,5 @@
 "use client";
-import StoreProvider from "./redux";
-import { Inter } from "next/font/google";
-import { useMemo } from "react";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default async function Home() {
-  const interClassName = useMemo(() => inter.className, []);
-
-  return (
-    <StoreProvider>
-      <>
-        <main className={interClassName}>HOME</main>
-      </>
-    </StoreProvider>
-  );
+export default function Home() {
+  return <div>HOME PAGE</div>;
 }
