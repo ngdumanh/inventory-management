e02527f8d50b8b2d49776ea2e278cffe03fbc4d4
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-09-28 15:13:10 -0500

    ok

--- client/src/app/profile/[id]/page.tsx
+++ client/src/app/profile/[id]/page.tsx
@@ -1,4 +1,10 @@
-export default function UserProfile({ params }: any) {
+interface UserProfileProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function UserProfile({ params }: UserProfileProps) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-center text-white text-2xl">User Profile</h1>
