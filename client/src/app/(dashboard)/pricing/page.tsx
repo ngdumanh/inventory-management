de63dc685a89125bf27e5705ef3494d3e2db41d6
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-09-30 02:36:48 -0500

    ok

--- client/src/app/(dashboard)/pricing/page.tsx
+++ client/src/app/(dashboard)/pricing/page.tsx
@@ -1,38 +1,7 @@
-'use client'
-
-// Component Imports
-import PricingWrapper from '@/views/front-pages/pricing'
-
-// Data Imports
-import { useLoginMutation } from '@/state/api'
-import { useState, useEffect } from 'react'
-
 const PricingPage = () => {
-  const [login, { isLoading, error }] = useLoginMutation()
-  const [email, setEmail] = useState('dasdas')
-  const [password, setPassword] = useState('ddsadsa')
-  const [result, setResult] = useState(null)
-
-  useEffect(() => {
-    const handleLogin = async () => {
-      try {
-        const result = await login({ email, password }).unwrap()
-        //setResult(result);
-        console.log('Login successful:', result)
-      } catch (err) {
-        console.error('Login failed:', err)
-      }
-    }
-
-    handleLogin()
-  }, [email, password, login])
-
   return (
     <div>
       <h1>Pricing ACB!</h1>
-      {isLoading && <p>Loading...</p>}
-      {/* {error && <p>Error: {error.message}</p>} */}
-      {result && <p>Login successful: {JSON.stringify(result)}</p>}
     </div>
   )
 }
