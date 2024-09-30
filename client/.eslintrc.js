1f68539828b57d48fa8747fa011dc1bddbd104a1
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-09-30 03:23:23 -0500

    ok

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -118,10 +118,18 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx', 'src/iconify-bundle/*'],
+      files: ['*.ts', '*.tsx', 'src/iconify-bundle/*', 'src/state/redux.tsx'],
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-var-requires': 'off'
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/consistent-type-imports': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'lines-around-comment': 'off',
+        'padding-line-between-statements': 'off',
+        'newline-before-return': 'off',
+        'import/newline-after-import': 'off',
+        'import/order': 'off',
+        '@typescript-eslint/ban-types': 'off'
       }
     }
   ]
