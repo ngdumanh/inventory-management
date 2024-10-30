74985336c1edca7bf91f58b33ebc53cd84dd0372
nguyennhutbinh <nguyennhutbinh@DESKTOP-VPQ5F6L>
2024-10-30 10:59:27 -0500

    ok

---
+++ client/src/app/[lang]/(blank-layout-pages)/(guest-only)/test/page.tsx
@@ -0,0 +1,14 @@
+// Next Imports
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Test',
+  description: 'test page'
+}
+
+const TestPage = () => {
+  // return test datas
+  return <div>TestPage</div>
+}
+
+export default TestPage
